74dbcdf97a4b57c6e3ee117379855d0562e36c66
Aleksandrs LEvinskis <a.levinskis@gmail.com>
2013-08-04 20:53:43 +0000

    using readFileSync

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
-
+var page = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(page.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
